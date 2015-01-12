404cf966c902dda0ddd34d25c532623e7661d573
Krystian Brazulewicz <kbrazulewicz@atlassian.com>
2015-01-12 13:42:54 +0100

    Unable to release via maven

--- README-atlassian.md
+++ README-atlassian.md
@@ -9,8 +9,4 @@ mvn clean install -DskipTests -Djdk16_home=$JAVA6_HOME -DdisableDistribution=tru
 How to release
 ==============
 
-export JAVA_HOME=$JAVA6_HOME
-export PATH=$JAVA_HOME/bin:$PATH
-
-mvn release:prepare -Darguments="-DskipTests -Djdk16_home=$JAVA6_HOME -DdisableDistribution=true" -DdisableDistribution=true
-mvn release:perform -Darguments="-DskipTests -Djdk16_home=$JAVA6_HOME -DdisableDistribution=true" -DdisableDistribution=true
+See atlassian-deploy.sh
--- atlassian-deploy.sh
+++ atlassian-deploy.sh
@@ -1,9 +1,27 @@
 #! /bin/sh
 
 TAG=3.6.10.Final-atlassian-4
-
 JAR_PATTERN="*${TAG}.jar"
 
+# mvn clean source:jar package -DskipTests -Djdk16_home=$JAVA_HOME -DdisableDistribution=true
+
+# deploy parent pom
+mvn deploy:deploy-file -Dfile=hibernate-parent/pom.xml -DpomFile=hibernate-parent/pom.xml \
+    -DrepositoryId=atlassian-3rdparty \
+    -Durl=https://maven.atlassian.com/3rdparty
+
+# deploy all the JARs with source JARs when they exist
 find . -name ${JAR_PATTERN} | while read jar; do
-    echo $jar
+    pom=`echo $jar | sed -e 's/\/target\/.*jar$/\/pom.xml/'`
+    sourceJar=`echo $jar | sed -e 's/\.jar$/-sources.jar/'`
+
+    DEPLOY_ARGS="-Dfile=${jar} -DpomFile=${pom}"
+
+    if [ -f ${sourceJar} ]; then
+        DEPLOY_ARGS="${DEPLOY_ARGS} -Dsources=${sourceJar}"
+    fi
+
+    mvn deploy:deploy-file ${DEPLOY_ARGS} \
+        -DrepositoryId=atlassian-3rdparty \
+        -Durl=https://maven.atlassian.com/3rdparty
 done
